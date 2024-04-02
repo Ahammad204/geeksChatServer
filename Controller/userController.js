c77e3c1195895bd05718ce49bbe6f32d80cdb23f
Ahammad204 <Ahammadcode@gmail.com>
2024-04-02 06:34:11 +0600

    use bcrypt for password

--- Controller/userController.js
+++ Controller/userController.js
@@ -4,7 +4,21 @@ const expressAsyncHandler = require("express-async-handler");
 const generateToken = require("../Config/generateToken");
 
 // Login Controller
-const loginController = () => {};
+const loginController = expressAsyncHandler(async (req, res) => {
+  const { name, password } = req.body;
+  const user = await UserModel.findOne({ name });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    throw new Error("Invalid userName Or password");
+  }
+});
 
 // Registration Controller
 const registerController = expressAsyncHandler(async (req, res) => {
--- Models/userModel.js
+++ Models/userModel.js
@@ -1,24 +1,38 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userModel = mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      requried: true,
     },
     email: {
       type: String,
-      required: true,
+      requried: true,
     },
     password: {
       type: String,
-      required: true,
+      requried: true,
     },
   },
   {
-    timeStamps: true,
+    timeStamp: true,
   }
 );
 
-const user = mongoose.model("User",userModel);
-module.exports = user;
+userModel.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userModel.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userModel);
+module.exports = User;
