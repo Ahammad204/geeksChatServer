7ec80c5a118b04b3550b1d59e597a40fa5121e48
Ahammad204 <Ahammadcode@gmail.com>
2024-04-02 17:25:54 +0600

    create fetch user

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -2,10 +2,13 @@ const express = require("express");
 const {
   loginController,
   registerController,
+  fetchAllUsersController,
 } = require("../Controller/userController");
+const { protect } = require("../Middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/login", loginController);
 router.post("/register", registerController);
+router.get("/fetchUsers", protect, fetchAllUsersController);
 
 module.exports = router;
